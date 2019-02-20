651c490ce634941536eee7a9158ed1650b559cec
C c <roshan90@hispeed.ch>
2019-02-20 21:19:38 +0100

    Added configurations and projects

---
+++ MyWpfApp/ViewModel/PatientViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWpfApp.ViewModel
+{
+    internal class PatientViewModel : INotifyPropertyChanged
+    {
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void HandleOnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
